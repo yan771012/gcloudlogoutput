36ae1e6d314fd169a1f99566fb86bd205602615c
hrian <rian@meepshop.com>
2016-04-10 03:37:38 +0800

    Update gcloudlogging.go

--- gcloudlogging.go
+++ gcloudlogging.go
@@ -173,7 +173,7 @@ func (clo *CloudLoggingOutput) SendRecord(name string, entries []*logging.LogEnt
 	}
 	e := &logging.WriteLogEntriesRequest{CommonLabels: labels, Entries: entries}
 	for i, v := range e.Entries {
-		log.Print(v)
+		log.Print(i,v)
 	}
 	_, err = clo.service.Projects.Logs.Entries.Write(clo.conf.ProjectID, name, e).Do()
 	if err != nil {
